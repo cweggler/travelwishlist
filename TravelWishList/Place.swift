dda8f93d07f696c477995e51facbbb039b87fb24
Cara Weggler <cweggler@gmail.com>
2019-03-01 07:34:07 -0600

    Created a Place class

---
+++ TravelWishList/Place.swift
@@ -0,0 +1,28 @@
+//
+//  Place.swift
+//  TravelWishList
+//
+//  Created by Cara on 3/1/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Place: NSObject {
+    var name: String
+    var hasVisited: Bool
+    var latitude: CLLocationDegrees
+    var longitude: CLLocationDegrees
+    // info about a place's location from CLLocationCoordinate2D for example
+    
+    init(name: String, hasVisited: Bool, latitude: CLLocationDegrees, longitude: CLLocationDegrees){
+        self.name = name
+        self.hasVisited = hasVisited
+        self.latitude = latitude
+        self.longitude = longitude
+        
+        super.init()
+    }
+}
+
