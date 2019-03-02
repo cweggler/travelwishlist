4b2e9a17c5b721a6b539805a1218a606949e7bc4
Cara Weggler <cweggler@gmail.com>
2019-03-02 15:39:26 -0600

    Created two viewcontrollers one for the map view and the other for the places

---
+++ TravelWishList/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  TravelWishList
+//
+//  Created by Cara on 3/2/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var mapView: MKMapView!
+}
---
+++ TravelWishList/PlacesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlacesViewController.swift
+//  TravelWishList
+//
+//  Created by Cara on 3/2/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+
+class PlacesViewController: UITableViewController {
+    
+}
